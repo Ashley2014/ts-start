c7efe7107b60c2b47822df5da6983b22905a1973
xiaoming <1746534101@qq.com>
2017-08-03 17:33:58 +0800

    i

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// import 'lodash';
+import 'jquery';
+// import { StringValidator } from "./c/a";
+console.log(12213);
+
+// console.log(_,$)
+// import jQuery = require('jquery');
+// import * as jQuery from "jquery";
+console.log(jQuery,myLib,aa)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "./dist/bundle.js",
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [ ".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: {
+              configFileName: path.join(__dirname, './', 'tsconfig.json'),
+            }
+          },
+        ]
+      },
+    ]
+
+    // preLoaders: [
+    //   // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+    //   { test: /\.js$/, loader: "source-map-loader" }
+    // ]
+  },
+
+  // Other options...
+};
